b4cfc5869afd1929718f98d0dd3785000aabaee6
jorge garcia <jorgearmando1999@hotmail.com>
2021-06-04 10:14:02 -0700

    Version 3

--- dist/jquery.min.b1f391c5.js
+++ dist/jquery.min.b1f391c5.js
@@ -4192,7 +4192,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51309" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59290" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
